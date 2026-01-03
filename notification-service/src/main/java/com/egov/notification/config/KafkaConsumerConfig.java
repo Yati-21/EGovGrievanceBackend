c4843959973d169ff7d0feaf532503de6516ef94
Yati-21 <yati.amity@gmail.com>
2026-01-03 12:10:38 +0530

    added dockerfiles

--- notification-service/src/main/java/com/egov/notification/config/KafkaConsumerConfig.java
+++ notification-service/src/main/java/com/egov/notification/config/KafkaConsumerConfig.java
@@ -3,6 +3,7 @@ package com.egov.notification.config;
 import com.egov.notification.event.GrievanceStatusChangedEvent;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
@@ -18,12 +19,15 @@ import java.util.Map;
 @Configuration
 public class KafkaConsumerConfig {
 
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+    
     @Bean
     public ConsumerFactory<String, GrievanceStatusChangedEvent> consumerFactory() {
 
         Map<String, Object> props = new HashMap<>();
 
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "notification-service");
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
