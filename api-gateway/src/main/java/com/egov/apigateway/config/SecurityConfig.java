f5e8a46b439d5c9d3bde9e1ba251f28950de05ad
Yati-21 <yati.amity@gmail.com>
2026-01-04 14:00:06 +0530

    added cors config

--- api-gateway/src/main/java/com/egov/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/egov/apigateway/config/SecurityConfig.java
@@ -1,9 +1,15 @@
 package com.egov.apigateway.config;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
@@ -12,15 +18,31 @@ public class SecurityConfig {
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
 
         return http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
                 .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
                 .authorizeExchange(ex -> ex
-               	    .pathMatchers("/auth/**").permitAll()
-               	    .pathMatchers("/reference/**").permitAll()
+                        .pathMatchers("/auth/**").permitAll()
+                        .pathMatchers("/reference/**").permitAll()
 //                	    .anyExchange().authenticated()
                 	    .anyExchange().permitAll()
                 		)
                 .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+        configuration.setAllowedHeaders(
+                Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "X-USER-ID", "X-USER-ROLE"));
+        configuration.setAllowCredentials(true);
+        configuration.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
