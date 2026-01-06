83ccadfb233dd9ff759984ad5866f69613b9eb49
Yati-21 <yati.amity@gmail.com>
2026-01-06 10:55:44 +0530

    allow netlify in cors

--- api-gateway/src/main/java/com/egov/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/egov/apigateway/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200","https://githubfrontendegov.netlify.app/"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(
                 Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "X-USER-ID", "X-USER-ROLE"));
